8df8f328b347b941e06801f612c8c027e2abd798
RamliSiburian <ramlisiburian1111@gmail.com>
2022-11-27 15:28:39 +0700

    update db

--- Pkg/Mysql/Mysql.go
+++ Pkg/Mysql/Mysql.go
@@ -1,7 +1,10 @@
 package Mysql
 
 import (
-	"gorm.io/driver/mysql"
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -10,8 +13,17 @@ var DB *gorm.DB
 func DatabaseInit() {
 	var err error
 
-	DBName := "root:@tcp(127.0.0.1:3306)/test_nutecth?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(DBName), &gorm.Config{})
+	// DBName := "root:@tcp(127.0.0.1:3306)/test_nutecth?charset=utf8mb4&parseTime=True&loc=Local"
+	// DB, err = gorm.Open(mysql.Open(DBName), &gorm.Config{})
+
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
