18a552b9ce325951d8815927c174fadf91987e07
RamliSiburian <ramlisiburian1111@gmail.com>
2022-11-28 08:23:27 +0700

    update

--- src/Component/Modal/Edit-Product.js
+++ src/Component/Modal/Edit-Product.js
@@ -1,37 +1,92 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Form, Modal } from 'react-bootstrap'
+import { Alert, Button, Form, Modal } from 'react-bootstrap'
 import * as Icon from 'react-icons/fa'
 import { useMutation, useQuery } from 'react-query'
 import { API } from '../../Config/Api'
+import { useNavigate } from 'react-router-dom';
+
 
 function EditProduct({ showEdit, setShowEdit, idEdit, dataEdit }) {
     const handleClose = () => setShowEdit(false)
-    const [preview, setPreview] = useState(dataEdit?.image);
-    // const [dataEdits, setDataEdit] = useState(dataEdit)
-    // console.log('ini data', dataEdits);
-    const setDataEdit = useState()
-
-    console.log(dataEdit?.nama);
+    const [preview, setPreview] = useState(null);
+    const [message, setMessage] = useState(null)
+    const navigate = useNavigate()
 
 
+
+
+    const [product, setProduct] = useState({
+        image: "",
+        nama: "",
+        harga_beli: 0,
+        harga_jual: 0,
+        stok: 0,
+    })
 
     const handleOnChange = (e) => {
-        // setDataEdit({
-        //     ...dataEdit,
-        //     [e.target.name]:
-        //         e.target.type === "file" ? e.target.files : e.target.value,
-        // })
-        // if (e.target.type === "file") {
-        //     let url = URL.createObjectURL(e.target.files[0]);
-        //     setPreview(url)
-        // }
-
-        // console.log(dataEdit);
+        setProduct({
+            ...product,
+            [e.target.name]:
+                e.target.type === "file" ? e.target.files : e.target.value,
+        })
+        if (e.target.type === "file") {
+            let url = URL.createObjectURL(e.target.files[0]);
+            setPreview(url)
+        }
     }
 
-
     const handleOnSubmit = useMutation(async (e) => {
-        alert("test")
+        try {
+            e.preventDefault();
+
+            const formData = new FormData();
+            formData.set("image", product.image[0], product.image[0].name);
+            formData.set("nama", product.nama);
+            formData.set("harga_beli", product.harga_beli);
+            formData.set("harga_jual", product.harga_jual);
+            formData.set("stok", product.stok);
+
+            const data = await API.patch("/Product/" + idEdit, formData, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`,
+                },
+            });
+            if (data.data.code === 200) {
+                const alert = (
+                    <Alert variant="success" className="py-1 fw-bold">
+                        Produk berhasil diedit
+                    </Alert>
+                )
+                refetchProduct()
+                setMessage(alert);
+                setPreview(null)
+                setProduct({
+                    image: "",
+                    nama: "",
+                    harga_beli: 0,
+                    harga_jual: 0,
+                    stok: 0,
+                })
+
+                const timer = setTimeout(navigates, 1000);
+
+                function navigates() {
+                    setMessage(null)
+                    setShowProduct(false)
+                    navigate("/Dashboard");
+                }
+
+            }
+        } catch (error) {
+
+            console.log(error.data.data.message);
+            const alert = (
+                <Alert variant="danger" className="py-1">
+                    {error.data.data.message}
+                </Alert>
+            )
+            setMessage(alert);
+        }
     })
 
 
@@ -41,7 +96,7 @@ function EditProduct({ showEdit, setShowEdit, idEdit, dataEdit }) {
                 <Modal.Title className='modal-title fs-1 fw-bold' style={{ color: "#ff4b00" }}>Edit Produk</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                {/* {message && message} */}
+                {message && message}
                 <Form onSubmit={(e) => handleOnSubmit.mutate(e)}>
                     <Form.Group className='w-100 mb-3' controlId="formBasicimage">
                         <Form.Label className="btn text-white" style={{
@@ -74,9 +129,9 @@ function EditProduct({ showEdit, setShowEdit, idEdit, dataEdit }) {
                         <Form.Control
                             type="text"
                             name="nama"
-                            placeholder="Nama Barang"
+                            placeholder={dataEdit?.nama}
                             onChange={handleOnChange}
-                            value={dataEdit?.nama}
+                            value={product?.nama}
                         />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicHargaBeli">
@@ -84,9 +139,9 @@ function EditProduct({ showEdit, setShowEdit, idEdit, dataEdit }) {
                         <Form.Control
                             type="number"
                             name="harga_beli"
-                            placeholder="Harga beli"
+                            placeholder={dataEdit?.harga_beli}
                             onChange={handleOnChange}
-                            value={dataEdit?.harga_beli}
+                            value={product?.harga_beli}
                         />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicHargaJual">
@@ -94,9 +149,9 @@ function EditProduct({ showEdit, setShowEdit, idEdit, dataEdit }) {
                         <Form.Control
                             type="number"
                             name="harga_jual"
-                            placeholder="Harga jual"
+                            placeholder={dataEdit?.harga_jual}
                             onChange={handleOnChange}
-                            value={dataEdit?.harga_jual}
+                            value={product?.harga_jual}
                         />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicStok">
@@ -104,9 +159,9 @@ function EditProduct({ showEdit, setShowEdit, idEdit, dataEdit }) {
                         <Form.Control
                             type="number"
                             name="stok"
-                            placeholder="Stok"
+                            placeholder={dataEdit?.stok}
                             onChange={handleOnChange}
-                            value={dataEdit?.stok}
+                            value={product?.stok}
                         />
                     </Form.Group>
                     <Button type='submit' className='btn text-white fw-bold link w-100 border-0' style={{ background: "#ff4b00" }}>Save</Button>
